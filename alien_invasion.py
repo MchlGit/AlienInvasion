0e350bea015a7e7d40d7c69c71b597fe5f580615
mixl5448 <mixl5448@gmail.com>
2020-04-23 22:16:02 -0700

    made rocket able to move back and forth without going off screen.

--- alien_invasion.py
+++ alien_invasion.py
@@ -12,9 +12,10 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
         pygame.init()
         self.settings = Settings()
-
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -23,17 +24,39 @@ class AlienInvasion:
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            
+            self._check_events()
+            self.ship.update()
+            self._update_screen()           
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN: 
+                self._key_updown_events(event, True)
+            elif event.type == pygame.KEYUP: 
+                self._key_updown_events(event, False)
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+
+    def _key_updown_events(self, event, isKeyDown=True):
+        if event.key == pygame.K_RIGHT: 
+            # Move the ship to the right. 
+            self.ship.moving_right = isKeyDown
+        elif event.key == pygame.K_LEFT: 
+            # Move the ship to the left. 
+            self.ship.moving_left = isKeyDown
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _update_screen(self):
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
